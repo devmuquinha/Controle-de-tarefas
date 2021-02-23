d42aa57b43000fac010715df4c5789b010ad2560
Matheus Moreira <matheusmoreira2004@live.com>
2021-02-23 14:21:55 -0300

    Banco atualizado e php quase finalizado

---
+++ tcc/db_tarefas_TCC.sql
@@ -0,0 +1,47 @@
+create database db_tarefas_TCC;
+use db_tarefas_TCC;
+
+create table tb_integrantes(
+tb_integrante_id integer not null auto_increment primary key,
+tb_integrante_nome varchar(100)not null
+);
+ 
+create table tb_tarefas(
+tb_tarefa_id integer not null auto_increment primary key,
+tb_tarefa_nome varchar(64)not null,
+tb_tarefa_descricao varchar(128)not null
+);
+
+create table tb_grupos(
+tb_grupo_id integer not null auto_increment primary key,
+tb_integrante_id integer,
+constraint fk_integrante_id foreign key(tb_integrante_id) REFERENCES tb_integrantes(tb_integrante_id),
+tb_tarefa_id integer not null,
+constraint fk_tarefa_id foreign key(tb_tarefa_id) REFERENCES tb_tarefas(tb_tarefa_id)
+);
+
+/*TESTES*/
+insert into tb_integrantes(tb_integrante_nome) value ("Matheus");
+insert into tb_tarefas(tb_tarefa_nome, tb_tarefa_descricao) value ("Criar logo", "Cria assim assado");
+insert into tb_integrantes(tb_integrante_nome) value ("Samuel");
+insert into tb_tarefas(tb_tarefa_nome, tb_tarefa_descricao) value ("Testa alguma coisa", "Testa assim assim assado");
+insert into tb_tarefas(tb_tarefa_nome, tb_tarefa_descricao) value ("Testa isso", "Testa daquele jeito");
+insert into tb_grupos(tb_integrante_id, tb_tarefa_id) value ("1", "1");
+insert into tb_grupos(tb_integrante_id, tb_tarefa_id) value ("2", "2");
+insert into tb_grupos(tb_integrante_id, tb_tarefa_id) value ("1", "3");
+insert into tb_grupos(tb_integrante_id, tb_tarefa_id) value ("2", "3");
+
+select * from tb_grupos;
+select * from tb_integrantes;
+select * from tb_tarefas;
+
+select tb_tarefas.tb_tarefa_id, tb_tarefas.tb_tarefa_nome, tb_tarefas.tb_tarefa_descricao, tb_integrantes.tb_integrante_nome 
+from tb_grupos
+inner join tb_tarefas
+on tb_grupos.tb_tarefa_id = tb_tarefas.tb_tarefa_id
+inner join tb_integrantes
+on tb_integrantes.tb_integrante_id = tb_grupos.tb_integrante_id
+order by tb_tarefas.tb_tarefa_id;
+
+/*drop database db_tarefas_tcc;*/
+/*TESTES*/
--- tcc/index.php
+++ tcc/index.php
@@ -15,37 +15,40 @@ echo '
 <h1>Adicionar tarefas TCC</h1>
 <br>
 <form  method="POST">';
-
-$select = "select * from tb_integrantes;";
-$result = $conexao->query($select);
-if ($result->num_rows > 0) {
-echo '<label for="integrantes">Escolha os integrantes:</label><br>';
-	while ($row = $result->fetch_assoc()) {
-		echo "<input type='checkbox' id = 'ckbx_" . $row['tb_integrante_nome'] ."'>". $row['tb_integrante_nome']. " <br>";
-	}; 
-	echo '<br>';
-}
 echo'
 	<br><br>
-	<textarea name="tarefa"class="input-tarefa"placeholder="ESCREVA AQUI A TAREFA DOS INTEGRANTES" ></textarea>
+	<textarea name="tarefa_nome" placeholder="ESCREVA AQUI O NOME DA TAREFA" ></textarea>
 	<br><br>
+	<textarea name="tarefa_descricao" class="input-tarefa" placeholder="ESCREVA AQUI A TAREFA DOS INTEGRANTES" ></textarea>
+	<br><br>';
+	$select = "select * from tb_integrantes;";
+	$result = $conexao->query($select);
+	if ($result->num_rows > 0) {
+	echo '<label for="integrantes">Escolha os integrantes:</label><br>';
+		while ($row = $result->fetch_assoc()) {
+			echo "<input type='checkbox' name = 'ckbx_integrantes[]' value='" . $row['tb_integrante_id'] ."'>". $row['tb_integrante_nome']. " <br>";
+		}; 
+		echo '<br>';
+	}
+	echo '
 	<input type="submit" name="btn_enviar" class="btn" value="ENVIAR" id="btn_enviar">
 	</div>
-</form>
-</center>';
-
+	</form>
+	</center>';
 
 if(isset($_POST['btn_enviar'])){ 
-
-$nome = $_POST['integrante'];
-$tarefa = $_POST['tarefa'];
+	
+$integrantes = $_POST['ckbx_integrantes'];
+$nome = $_POST['tarefa_nome'];
+$descricao = $_POST['tarefa_descricao'];
 try 
 {
-	for ($qtd_integrantes = 0; $qtd_integrantes != str_split(implode(" ", $row), ''); $qtd_integrantes)
+	foreach ($integrantes as $integrante)
 	{
-	$sql = mysqli_query($conexao,"INSERT INTO tb_integrantes (tb_integrante_nome) VALUE ('$nome');");
-	echo "Inserido";
+		echo $integrante . '<br>';
 	}
+	//$sql = mysqli_query($conexao, "INSERT INTO tb_tarefas (tb_integrante_nome, tb_tarefa_descricao, tb_integrante_id) VALUES ('$nome', '$descricao', '$integrante');");
+	echo "Inserido";
 }
 catch (Exception $err)
 {
