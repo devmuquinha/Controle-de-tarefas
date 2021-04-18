0553e99a5b8d1a339db59498b714f408064cce34
Lovato17 <79473205+Lovato17@users.noreply.github.com>
2021-04-18 01:02:36 -0300

    HREF DA PAGINA
    ATUALIZADO

--- tcc/index.php
+++ tcc/index.php
@@ -4,9 +4,15 @@
         <title>TCC</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="index.css">
+        <link
+            rel="stylesheet"
+            href="css-da-pagina/indexmaior.css"
+            media="screen and (min-width:1125px)"/>
     </head>
     <body class="body">
+        <h1 class="h" style="font-family: Snap ITC;">TAREFA</h1>
         <a href="tarefas.php"><img class="seta" src="img/seta.png" alt=""></a>
+
     <?php
 error_reporting(E_ALL);
 session_start();
@@ -18,17 +24,15 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION
   } else {
 ?>
         <center>
-            <div class="form"><br>
-                <h1 class="h" style="font-family: Snap ITC;">TAREFA</h1>
+            <div class="form">
+
                 <form method="POST">
                     <input class="titulo-tarefa" name="tarefa_nome" placeholder="Titulo da Tarefa">
-
                     <textarea
                         name="tarefa_descricao"
                         class="desc-tarefa"
                         placeholder="Descriçao da Tarefa"></textarea>
-                    <br>
-                    <br>
+
                     <?php
     $tarefa->puxarIntegrantes();
     
@@ -39,8 +43,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION
                         class="btn"
                         value="ENVIAR"
                         id="btn_enviar">
-                    <br>
-                    <br>
+
                 </form>
             <?php
 
@@ -56,7 +59,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION
                 $tarefaId = $tarefa->pegaIdTarefa();
                 $tarefa->fazerInsertGrupos($integrantes, $tarefaId);
             } else {
-                echo '<div class="log"><label >Preencha todos os campos!</label ></div>';
+                echo '<div class="alert- log"><label >Preencha todos os campos!</label ></div>';
             }
     }
         echo'
--- tcc/login.php
+++ tcc/login.php
@@ -3,25 +3,31 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <link rel="stylesheet" href="css-da-pagina/login.css">   
-        <meta name="viewport"  content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css-da-pagina/login.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login</title>
     </head>
-    <body class="body" >
+    <body class="body">
         <center>
             <div class="form-box">
-                <div class="box-lado">       
+                <div class="box-lado">
                     <img class="img" src="img/user.png" alt="">
                     <form class="for" method="post">
-                        <h1 class="h">Login</h1>                        <input type="text"  class="name" placeholder="Nome" name="txt_login"/>
+                        <h1 class="h">Login</h1>
+                        <input type="text" class="name" placeholder="Nome" name="txt_login"/>
                         <input type="password" class="bt" placeholder="Senha" name="txt_senha"/><br><br>
-                        <input type="submit" class="button" name="btn_logar" value="Logar" id="btn_enviar">
+                        <input
+                            type="submit"
+                            class="button"
+                            name="btn_logar"
+                            value="Logar"
+                            id="btn_enviar">
                     </form>
-                </div>  
+                </div>
                 <!--<label class="label" >FAÇA LOGIN PARA<br> VER AS TAREFAS</label> -->
             </div>
         </center>
-<?php
+        <?php
     require_once 'TarefaDAO.php';
     $tarefa = new Tarefa;
     session_start();
@@ -34,6 +40,8 @@
         $senha = $_POST['txt_senha'];
         $tarefa->fazerLogin($login, $senha);
     }
+  
+    //echo $_SERVER['REMOTE_ADDR'];
     ?>
     </body>
 </html>
