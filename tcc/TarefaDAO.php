70e6b433ffe61f87be446af82bbb97b949489571
Matheus Moreira <matheusmoreira2004@live.com>
2021-04-23 13:04:34 -0300

    Update TarefaDAO.php

--- tcc/TarefaDAO.php
+++ tcc/TarefaDAO.php
@@ -168,7 +168,6 @@ class tarefa
 
         $login = mysqli_real_escape_string($conexao, $login);
         $senha = mysqli_real_escape_string($conexao, $senha);
-        $senha = md5($senha);
         $selectLogin =  "SELECT * FROM tb_integrantes WHERE tb_integrantes.tb_integrante_nome = '$login' AND tb_integrantes.tb_integrante_senha = '$senha';";
 
         $resultado = mysqli_query($conexao, $selectLogin);
