e6ef784e06c7734adae0a30f51aa60431358dfec
Matheus Moreira <matheusmoreira2004@live.com>
2021-05-10 21:07:13 -0300

    Finalizado
    Trabalho foi finalizado com algumas remoções de arquivos .css não utilizados

--- tcc/index.php
+++ tcc/index.php
@@ -1,71 +1,63 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>TCC</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="index.css">
-        <link
-            rel="stylesheet"
-            href="css-da-pagina/indexmaior.css"
-            media="screen and (min-width:1125px)"/>
-    </head>
-    <body class="body">
-        <h1 class="h" style="font-family: Snap ITC;">TAREFA</h1>
-        <a href="tarefas.php"><img class="seta" src="img/seta.png" alt=""></a>
+
+<head>
+    <title>TCC</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="css-da-pagina/indexmaior.css" media="screen and (min-width:1125px)" />
+</head>
+
+<body class="body">
+    <h1 class="h" style="font-family: Snap ITC;">TAREFA</h1>
+    <a href="tarefas.php"><img class="seta" src="img/seta.png" alt=""></a>
 
     <?php
-error_reporting(E_ALL);
-session_start();
-require_once 'TarefaDAO.php';
-$tarefa = new Tarefa;
-if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION['login'] != 'Eduardo') {
-    header("location: tarefas.php");
-    exit;
-  } else {
-?>
+    error_reporting(E_ALL);
+    session_start();
+    require_once 'TarefaDAO.php';
+    $tarefa = new Tarefa;
+    if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION['login'] != 'Eduardo') {
+        header("location: tarefas.php");
+        exit;
+    } else {
+    ?>
         <center>
             <div class="form">
 
                 <form method="POST">
                     <input class="titulo-tarefa" name="tarefa_nome" placeholder="Titulo da Tarefa">
-                    <textarea
-                        name="tarefa_descricao"
-                        class="desc-tarefa"
-                        placeholder="Descriçao da Tarefa"></textarea>
+                    <textarea name="tarefa_descricao" class="desc-tarefa" placeholder="Descriçao da Tarefa"></textarea>
 
                     <?php
-    $tarefa->puxarIntegrantes();
-    
-?>
-                    <input
-                        type="submit"
-                        name="btn_enviar"
-                        class="btn"
-                        value="ENVIAR"
-                        id="btn_enviar">
+                    $tarefa->puxarIntegrantes();
+
+                    ?>
+                    <input type="submit" name="btn_enviar" class="btn" value="ENVIAR" id="btn_enviar">
 
                 </form>
             <?php
 
-        if (isset($_POST['btn_enviar'])) {
-            $nome = $_POST['tarefa_nome'];
-            $descricao = $_POST['tarefa_descricao'];
-            $nome = mysqli_real_escape_string($conexao, $nome);
-            $descricao = mysqli_real_escape_string($conexao, $descricao);
-            
-            if (isset($_POST['ckbx_integrantes']) && $nome != '' && $descricao != '') {
-                $integrantes = $_POST['ckbx_integrantes'];
-                $tarefa->insertTarefa($nome, $descricao);
-                $tarefaId = $tarefa->pegaIdTarefa();
-                $tarefa->fazerInsertGrupos($integrantes, $tarefaId);
-            } else {
-                echo '<div class="alert- log"><label >Preencha todos os campos!</label ></div>';
+            if (isset($_POST['btn_enviar'])) {
+                $nome = $_POST['tarefa_nome'];
+                $descricao = $_POST['tarefa_descricao'];
+                $nome = mysqli_real_escape_string($conexao, $nome);
+                $descricao = mysqli_real_escape_string($conexao, $descricao);
+
+                if (isset($_POST['ckbx_integrantes']) && $nome != '' && $descricao != '') {
+                    $integrantes = $_POST['ckbx_integrantes'];
+                    $tarefa->insertTarefa($nome, $descricao);
+                    $tarefaId = $tarefa->pegaIdTarefa();
+                    $tarefa->fazerInsertGrupos($integrantes, $tarefaId);
+                } else {
+                    echo '<div class="alert- log"><label >Preencha todos os campos!</label ></div>';
+                }
             }
-    }
-        echo'
+            echo '
         </div>
         </center>';
-  }
-?>
-            </body>
-        </html>
+        }
+            ?>
+</body>
+
+</html>
--- tcc/tarefas.php
+++ tcc/tarefas.php
@@ -1,36 +1,28 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="css-da-pagina/tarefa.css">
-        <link
-            rel="stylesheet"
-            href="css-da-pagina/tarefamenorr.css"
-            media="screen and (max-width: 1225px)"/>
-        <link
-            rel="stylesheet"
-            href="css-da-pagina/tarefamaior.css"
-            media="screen and (min-width: 1125px)"/>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="css-da-pagina/tarefa.css">
+  <link rel="stylesheet" href="css-da-pagina/tarefamenorr.css" media="screen and (max-width: 1225px)" />
+  <link rel="stylesheet" href="css-da-pagina/tarefamaior.css" media="screen and (min-width: 1125px)" />
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link
-            href='https://fonts.googleapis.com/css?family=Open+Sans'
-            rel='stylesheet'
-            type='text/css'>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 
-        <title>Tarefas</title>
-    </head>
-    <body class="body">
+  <title>Tarefas</title>
+</head>
 
-        <form action="" method="POST">
-            <input type='submit' class="quit" name='btn_sair' value='Sair'>
+<body class="body">
 
-        </form>
-        <label for="" class="tarefa">Tarefas</label>
-    <?php
+  <form action="" method="POST">
+    <input type='submit' class="quit" name='btn_sair' value='Sair'>
+
+  </form>
+  <label for="" class="tarefa">Tarefas</label>
+  <?php
   session_start();
   require_once 'TarefaDAO.php';
   $tarefa = new Tarefa;
@@ -41,7 +33,7 @@
   } else {
     echo "<form class='form_task' method='post'>";
     $tarefa->puxarTarefas();
-   
+
     echo "</form>
     
     
@@ -61,14 +53,13 @@
       echo '<div class="alert-danger">Nenhuma tarefa selecionada!';
     }
   }
-  
+
   ?>
-    </div>
-    <center>
-        <br>
-        <a
-            href="https://drive.google.com/drive/u/1/folders/1dZbn48AqZq5DBivuVo9tV4PG1LGxPz5F"
-            class="drive">ATA'S JÁ CONCLUIDAS</a><br><br>
-    </center>
+  </div>
+  <center>
+    <br>
+    <a href="https://drive.google.com/drive/u/1/folders/1dZbn48AqZq5DBivuVo9tV4PG1LGxPz5F" class="drive">ATA'S JÁ CONCLUIDAS</a><br><br>
+  </center>
 </body>
+
 </html>
